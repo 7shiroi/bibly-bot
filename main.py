0b73a9ef4a154b0124c72e51b6eaafd6535d7217
7shiroi <lucky7kelvin@gmail.com>
2022-06-06 17:41:59 +0800

    feat: change to api.scripture

--- main.py
+++ main.py
@@ -2,19 +2,21 @@ from email import contentmanager
 import discord
 import os
 import requests
+import numpy as np
 
 from dotenv import load_dotenv
 from discord.ext import commands
+from urllib.parse import urlencode, quote
 
 bot = discord.Client()
 
 load_dotenv()
 DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
 BIBLE_HEADERS_KEY = os.getenv("BIBLE_HEADERS_KEY")
-BIBLE_HEADERS_HOST = os.getenv("BIBLE_HEADERS_HOST")
 
-bibleApi = 'https://ajith-holy-bible.p.rapidapi.com'
-bibleHeaders = {"X-RapidAPI-Host" : BIBLE_HEADERS_HOST, "X-RapidAPI-Key" : BIBLE_HEADERS_KEY}
+bibleApi = 'https://api.scripture.api.bible/v1'
+bibleHeaders = {"api-key" : BIBLE_HEADERS_KEY}
+
 
 # EVENT LISTENER FOR WHEN THE BOT HAS SWITCHED FROM OFFLINE TO ONLINE.
 @bot.event
@@ -25,21 +27,15 @@ async def on_ready():
 @bot.event
 async def on_message(message):
 	if message.content.split()[0] == "bible":
-		# bible john 3:16 / john 3:16-17
-		book = message.content.split()[1]
-		chapter = message.content.split()[2].split(':')[0]
-		verse = message.content.split()[2].split(':')[1]
-
 		response = ''
 
-		if '-' in verse:
-			queryParams = {"Book" : book, "chapter" : chapter, "VerseFrom" : verse.split('-')[0], "VerseTo" : verse.split('-')[1]}
-			response = requests.request("GET", bibleApi + "/GetVerses", headers=bibleHeaders, params=queryParams)
-		else:
-			queryParams = {"Book" : book, "chapter" : chapter, "Verse" : verse}
-			response = requests.request("GET", bibleApi + "/GetVerseOfaChapter", headers=bibleHeaders, params=queryParams)
+		reference = np.array(message.content.split())
+		queryString = {'query' : " ".join(reference[1:])}
+		print(urlencode(queryString, quote_via=quote))
+		response = requests.request("GET", bibleApi + "/bibles/2dd568eeff29fb3c-02/search?" +urlencode(queryString, quote_via=quote), headers=bibleHeaders)
 
-		await message.channel.send(response.json().get("Output"))
+		print(response.json().get("data").get("passages")[0].get("content"))
+		await message.channel.send(response.json().get("data").get("passages")[0].get("content"))
 
 # EXECUTES THE BOT WITH THE SPECIFIED TOKEN.
 bot.run(DISCORD_TOKEN)
